9e84ef620a7a28a5cf193bf79ee3d3f4dd10dddc
tuure <tuure.piitulainen@gmail.com>
2020-02-21 16:56:55 +0200

    Make counter unusable with no saldo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const Player = (props) => {
+  const [points, setPoints] = useState(20);
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={() => {
+        if (points > 0) {
+          props.increaseCounter()
+          setPoints(points - 1)
+        }
+      }}>
         Increase
       </button>
+      <p> You have { points } points </p>
+    </div>
+  )
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCounter = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
       <p> { count } </p>
+      <Player increaseCounter={increaseCounter}></Player>
     </div>
   )
 }
